4f0a7429b0020747074dfd34ae1e61e689c67552
Monika Zając <36509633+mnkzjc@users.noreply.github.com>
2019-11-10 15:20:20 +0100

    Add files

---
+++ components/Contact.js
@@ -0,0 +1,24 @@
+var Contact = React.createClass({
+    propTypes: {
+      item: React.PropTypes.object.isRequired,
+    },
+  
+    render: function() {
+      return (
+        React.createElement('div', {className: 'contactItem'},
+          React.createElement('img', {
+            className: 'contactImage',
+            src: 'https://image.flaticon.com/icons/svg/1312/1312130.svg'
+          }),
+          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+            this.props.item.email
+          )
+        )
+      )
+    },
+  });
+
+
+  export default Contact;
